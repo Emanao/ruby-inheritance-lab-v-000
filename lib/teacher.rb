ae35c603cba42b27ee178e74f4a791cf1f9c8036
Eva Navarro <emanao@me.com>
2019-11-15 17:29:37 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,8 +1,9 @@
 class Teacher < User
   attr_reader :students
   @@KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  @@all = []
   def initialize
-    @students = []
+    self.save
   end
   def knowledge
     @@KNOWLEDGE
@@ -12,5 +13,8 @@ class Teacher < User
   end
   def teach
     self.knowledge.sample
+  end
+  def save
+    @@all<<self
   end
 end
