6f143d350eb33eb77bbc11d90b1412463dcf4f37
Eva Navarro <emanao@me.com>
2019-11-15 17:30:51 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Teacher < User
   attr_reader :students
   @@KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
@@ -9,6 +10,7 @@ class Teacher < User
     @@KNOWLEDGE
   end
   def add_student(student)
+    binding.pry
     self.students<<student
   end
   def teach
